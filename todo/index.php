2badeb287b50837a20b44e06a484f597cc9be317
Zachary Freed <zfreed125@gmail.com>
2020-10-02 11:45:22 -0500

    refactor(todo): add venue and hover data

--- todo/index.php
+++ todo/index.php
@@ -5,41 +5,44 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
-          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <style>
-    input{
+    input {
         width: 75px;
         margin-left: 20px;
     }
-    h3{
+
+    h3 {
         margin-left: 160px;
     }
-    .wrapper{
-        display: flex;  
+
+    .wrapper {
+        display: flex;
         margin-left: 160px;
 
     }
-    .card{
+
+    .card {
         background-color: #4A0C57;
-        color:#4FDC0E;
+        color: #4FDC0E;
         border: solid 6px #4FDC0E;
         position: relative;
         margin: 20px;
         padding: 10px;
         max-width: 35%;
         text-align: center;
-	    flex-flow: row wrap;
+        flex-flow: row wrap;
 
     }
-    .container > div{
 
-    }
+    .container>div {}
+
     .dot {
         height: 25px;
         width: 25px;
@@ -48,160 +51,35 @@
         display: inline-block;
     }
 </style>
+
 <body>
-<form name="myForm" action="index.php" method="POST">
-<span>
-    <h3 class="center">(Client Only) Month: <input name="month" type="text" value="<?php echo $month; ?>"> Year: <input name="year" type="text" value="<?php echo $year; ?>"><input class="btn btn-primary" type="submit" name="submit" value="Submit"></h3>
-      
-</span>    
-</form>
+    <form name="myForm" action="index.php" method="POST">
+        <span>
+            <h3 class="center">(Client Only) Month: <input name="month" type="text" value="<?php echo $month; ?>"> Year: <input name="year" type="text" value="<?php echo $year; ?>"><input class="btn btn-primary" type="submit" name="submit" value="Submit"></h3>
+
+        </span>
+    </form>
     <div class='wrapper'>
 
-<?php
+        <?php
 
-require_once './config.php';
+        require_once './config.php';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$contact_sql = "SELECT id, CONCAT(firstname, ' ', lastname) as fullname FROM contacts ORDER BY lastname ASC, firstname ASC;";
-$contact_result = mysqli_query($conn, $contact_sql);
-$contacts_array = array();
-while ($row = mysqli_fetch_assoc($contact_result)) {
-    $contacts_array[] = array('id' => $row['id'], 'fullname' => $row['fullname']);
-}
-$venue_name_sql = "SELECT id, venueName FROM venues;";
-$result = mysqli_query($conn, $venue_name_sql);
-$venue_name_array = array();
-while ($row = mysqli_fetch_assoc($result)) {
-    $venue_name_array[] = array('id' => $row['id'], 'venueName' => $row['venueName']);
-}
-$client_booking_count_array = [];
-$sql = "SELECT clientNameId,venueNameId, MONTH(bookingDateTimeStart) as bookingMonth,YEAR(bookingDateTimeStart) as bookingYear FROM bookings ;";
-
-$result = mysqli_query($conn, $sql);
-while ($row = mysqli_fetch_assoc($result)){
-  
-  $clientNameId = $row['clientNameId'];
-  $venueNameId = $row['venueNameId'];
-  $bookingMonth = $row['bookingMonth'];
-  $bookingYear = $row['bookingYear'];
-
-foreach ($venue_name_array as $venue_item) {
-    if($row['venueNameId'] == $venue_item['id']){
-        $venueName = $venue_item['venueName'];
-    }
-}
-  $sql_contact = "SELECT bookingColor, bookingCount as bookingCountTotal FROM contacts WHERE id='$clientNameId';";
-  $result_client = mysqli_query($conn, $sql_contact);
-  while ($crow = mysqli_fetch_assoc($result_client)){
-    $bookingCountTotal = $crow['bookingCountTotal'];
-    $bookingColor = $crow['bookingColor'];
-  }
-    $client_booking_count_array[] = array(
-      'contactId' => $clientNameId,
-      'venuename' => $venueName,
-      'bookingColor' => $bookingColor,
-      'bookingMonth' => $bookingMonth,
-      'bookingYear' => $bookingYear,
-      'bookingCountTotal' => $bookingCountTotal
-    );
-    
-}
-    echo "<pre>";
-    // print_r($client_booking_count_array);
-    echo "</pre>";
-
-
-$todo_array = array();
-$client_booking_count = array();
-
-foreach ($client_booking_count_array as $booking) {
-    if ($booking['bookingYear'] == $year) {
-        foreach ($contacts_array as $contact_item) {
-            if($booking['contactId'] == $contact_item['id']){
-                // array_push($client_booking_count,$contact_item['id']);
-                $todo_array[$contact_item['id']] = array(
-                    'fullname' => $contact_item['fullname'],
-                    'bookingColor' => $booking['bookingColor'],
-                    'bookingCountTotal' => $booking['bookingCountTotal'],
-                    'bookings' => array_count_values($client_booking_count)[$contact_item['id']]);
-                    // 'bookings' => 0);
-                    
-            }
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $database);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
         }
-        if ($booking['bookingMonth'] == $month) {
-            foreach ($contacts_array as $contact_item) {
-                if($booking['contactId'] == $contact_item['id']){
-                    array_push($client_booking_count,$contact_item['id']);
-                    $todo_array[$contact_item['id']] = array(
-                        'fullname' => $contact_item['fullname'],
-                        'bookingColor' => $booking['bookingColor'],
-                        'bookingCountTotal' => $booking['bookingCountTotal'],
-                        'bookings' => array_count_values($client_booking_count)[$contact_item['id']]);
-                        
-                }
-            }
-        } // If Month
-    }else{ // If Year
-    foreach ($contacts_array as $contact_item) {
-            if($booking['contactId'] == $contact_item['id']){
-                // array_push($client_booking_count,$contact_item['id']);
-                $todo_array[$contact_item['id']] = array(
-                    'fullname' => $contact_item['fullname'],
-                    'bookingColor' => $booking['bookingColor'],
-                    'bookingCountTotal' => $booking['bookingCountTotal'],
-                    'bookings' => array_count_values($client_booking_count)[$contact_item['id']]);
-                    // 'bookings' => 0);
-                    
-            }
-        }
-    }
-}   
-    echo "<pre>";
-    // echo print_r($client_booking_count_array);
-    // echo print_r($todo_array);
-    // goal: bookings created/bookings required for client in selected month
-    // createdBookings, requiredBookings, clientName, bookingColor, venueName
-
-    // select bookings.clientNameId, bookings.venueNameId, bookings.bookingDateTimeStart
-    // select contacts.bookingCount, contacts.bookingColor, contacts.firstname, 
-    // contacts.lastname from contacts, bookings;
-
-
-    echo "</pre>";
-    foreach ($todo_array as $todo_item) {
-        $card = " <div class='card'><div class='container'> ";
-        $card .= " <h4><b>".$todo_item['fullname']."</b></h4> <!-- <p>Venue Name</p> --> ";
-        
-        for ($x = 1; $x <= $todo_item['bookingCountTotal']; $x++) {
-            if($x > $todo_item['bookings']){
-                $card .= "<div><span style='background-color: grey;' class='dot'></span></div>";
-            }else{
-                $card .= "<div><span style='background-color: ".$todo_item['bookingColor'].";' class='dot'></span></div>";
-            }
-        } 
-        $card .= " </div> </div> ";
-        echo $card;
-        }
-
-?>
-</div>
-</body>
-</html>
-
-<!-- 
+        $init_todo_array = array();
+        $todo_sql = "
 select 
-    concat(contacts.firstname, ' ', contacts.lastname) as client,
-    bookings.bookingDateTimeStart as startTime,
+    IFNULL(concat(contacts.firstname, ' ', contacts.lastname),'N/A') as client,
+    IFNULL(bookings.bookingDateTimeStart,'unset') as startTime,
     bookings.bookingLength as duration,
     contacts.bookingCount as requiredCount,
     contacts.bookingColor as color,
-    venues.venueName as venue
+    IFNULL(venues.venueName,'unassigned') as venue
 from 
     bookings
 left join contacts on bookings.clientNameId = contacts.id
@@ -209,8 +87,135 @@ left join venues on bookings.venueNameId = venues.id
 where
     bookings.bookingDateTimeStart is null or
     (
-        year(bookings.bookingDateTimeStart)=2020 and
-        month(bookings.bookingDateTimeStart)=9
+        year(bookings.bookingDateTimeStart)=" . $year . " and
+        month(bookings.bookingDateTimeStart)=" . $month . "
     )
-order by client;
- -->
+order by client
+;";
+
+        $todo_result = mysqli_query($conn, $todo_sql);
+        while ($row = mysqli_fetch_assoc($todo_result)) {
+            $init_todo_array[] = $row;
+        }
+        // Array
+        // (
+        //     [0] => Array
+        //         (
+        //             [client] => Arlen Schumer
+        //             [startTime] => unset
+        //             [duration] => 0
+        //             [requiredCount] => 4
+        //             [color] => #000000
+        //             [venue] => unassigned
+        //         )
+        // )
+
+        // $client_bookings = [
+        //     'client name' => [
+        //         'bookings' => [
+        //             ['startdate', 'duration', 'venue'],
+        //             ['startdate', 'duration', 'venue'],
+        //             ['startdate', 'duration', 'venue'],
+        //         ],
+        //         'color' => '#fff',
+        //         'requiredCount' => 'required',
+        //     ],
+        //     'client name' => [
+        //         'bookings' => [
+        //             ['startdate', 'duration', 'venue'],
+        //             ['startdate', 'duration', 'venue'],
+        //             ['startdate', 'duration', 'venue'],
+        //         ],
+        //         'color' => '#fff',
+        //         'requiredCount' => 'required',
+        //     ]
+        // ]
+        $data = $init_todo_array;
+        $client_bookings = [];
+        // echo "<pre>";
+        foreach ($data as $item) {
+            $client = $item['client'];
+
+            if (array_key_exists($client, $client_bookings)) {
+                array_push($client_bookings[$client]['bookings'], [
+                    'startTime' => $item['startTime'],
+                    'duration' => $item['duration'],
+                    'venue' => $item['venue']
+                ]);
+            } else {
+                $client_bookings[$client] = [
+                    'bookings' => [
+                        [
+                            'startTime' => $item['startTime'],
+                            'duration' => $item['duration'],
+                            'venue' => $item['venue']
+                        ]
+                    ],
+                    'color' => $item['color'],
+                    'requiredCount' => $item['requiredCount'],
+                    'htmlCard' => <<<HTML
+    <div class="card">
+        <div class="container">
+            <h4>$client</h4>
+        </div>
+    </div>
+HTML
+                ];
+            }
+        }
+        // print_r($client_bookings);
+        // echo "</pre>";
+        foreach($client_bookings as $client => $details){
+            // $bookings_count = sizeof($details['bookings']);
+            // echo $details['htmlCard'];
+        }
+
+
+
+        $client_count_array = array();
+        $full_client_count_array = array();
+        foreach ($init_todo_array as $item) {
+            $client = $item['client'];
+            $full_client_count_array[$client][] = $item;
+            $client_count_array[$client] = array_count_values(array_column($init_todo_array, 'client'))[$client];
+            $tt = array_unique($client_count_array);
+        }
+
+        $card = '';
+        foreach (array_keys($full_client_count_array) as $client) {
+            $card = " <div class='card'><div class='container'> ";
+            $card .= " <h4><b>" . $client . "</b>";
+            for ($i = 0; $i < count($full_client_count_array[$client]); $i++) {
+                $req = $full_client_count_array[$client][$i]['requiredCount'];
+                $card .= "<div><span title='" . 'StartTime: ' . $full_client_count_array[$client][$i]['startTime'] . ' Venue: ' . $full_client_count_array[$client][$i]['venue'] . "' style='background-color: " . $full_client_count_array[$client][$i]['color'] . ";' class='dot'></span></div>";
+            }
+            $empty_dots = $req - $i;
+            for ($j = 0; $j < $empty_dots; $j++) {
+                $card .= "<div><span style='background-color: grey;' class='dot'></span></div>";
+            }
+
+            $card .= " </div> </div> ";
+            echo $card;
+
+        }
+
+
+
+
+        // foreach(client_bookings as client => details){
+        // bookings = details['bookings]
+        // color = details['color']
+        // requiredCount = details['requiredCount']
+        // $bookings_count = len($bookings)
+        // foreach(bookings){
+        // 
+        // }
+        // 
+        // 
+        // }
+
+        ?>
+    </div>
+</body>
+
+</html>
